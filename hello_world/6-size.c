9bce04eb2d1bc48ed668e9c4458c7268a2fc96b8
alejandrodgz <5199@holbertonschool.com>
2022-06-13 19:40:41 -0700

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %ld", sizeof(char));
+	printf("Size of a int: %ld", sizeof(int));
+	printf("Size of a long: %ld", sizeof(long));
+	printf("Size of a float: %ld", sizeof(float));
+	return (0);
+}
