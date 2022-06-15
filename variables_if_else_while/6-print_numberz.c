8c17b607f8c08ee2199236491aa799c4d877fde4
alejandrodgz <5199@holbertonschool.com>
2022-06-14 21:44:09 -0500

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar (a);
+		a++;
+	}
+	putchar ('\n');
+	return (0);
+}
