c07eff19350ad345bcd7ca171184dd3ceb392e42
alejandrodgz <5199@holbertonschool.com>
2022-06-16 14:04:28 -0500

    task 5

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -40,4 +40,12 @@ int _islower(int c);
 
 int _isalpha(int c);
 
+/**
+ * print_sign - checks for signs in numbers
+ * @n: number to check
+ * Return: 1, -1, 0 depends on the sign
+ */
+
+int print_sign(int n);
+
 #endif
