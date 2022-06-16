624e11ef9eaef73efcf581b6a71f8887b3d0ee3f
alejandrodgz <5199@holbertonschool.com>
2022-06-16 11:39:14 -0500

    a

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,17 +4,24 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @int: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and error is set appropriately.
  */
 
 int _putchar(int);
+
 /**
  * print_alphabet - print the alphabet lowercase but is fucking hard to get it
  */
 
 void print_alphabet(void);
+
+/**
+ * void print_alphabet_x10 - Write a function that prints 10 times the alphabet
+ */
+
+void print_alphabet_x10(void);
 
 #endif
