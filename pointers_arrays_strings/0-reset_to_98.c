4a038117584b3ce20b0abc375cf93c00e6cc2e8b
alejandrodgz <5199@holbertonschool.com>
2022-06-22 12:47:15 -0500

    task 9

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -6,10 +6,21 @@
  * @n: pointer
  */
 
+
 void reset_to_98(int *n)
 {
-	int *pn = NULL;
+	int *p;
+	p = n;
+	*p = 98;
+}
 
-	pn = n;
-	*pn = 98;
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
 }
--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,19 @@
 
 char *_strcpy(char *dest, char *src)
 {
+	int len, a, b;
+	char tmp[98];
 
-	strcpy(dest, src);
+	len = strlen(src);
+
+	for (a = 0 ; a < len; a++)
+	{
+		tmp[a] = src[a];
+	}
+	for (b = 0; b < len; b++)
+	{
+		dest[b] = tmp[b];
+	}
 
 	return (dest);
 }
