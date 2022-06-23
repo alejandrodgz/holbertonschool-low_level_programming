c7ca484e9386d308a0dde00fed81e156ccf80ff9
alejandrodgz <5199@holbertonschool.com>
2022-06-22 23:36:06 -0500

    task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print a line with n c length
+ *
+ * length
+ * Return: character
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			i++;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			i++;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			i++;
+		}
+		else
+		{
+		printf("%d ", i);
+		i++;
+		}
+	}
+	printf ("\n");
+	return (0);
+}
