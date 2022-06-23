71478601353418e58429e0c5052b7102e103471d
alejandrodgz <5199@holbertonschool.com>
2022-06-23 15:39:53 -0500

    task0

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - print a line with n c length
+ *
+ * @size: length
+ * Return: character
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	i = 0;
+	j = 0;
+	k = 1;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	while ( i < size)
+	{
+		while(j <= i)
+		{
+			while (k < (size - i))
+			{
+				_putchar(32);
+				k++;
+			}
+			_putchar(35);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+		j = 0;
+		k = 1;
+	}
+}
