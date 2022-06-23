90762c199a1bed73068bf1a06176b1736c761bf0
alejandrodgz <5199@holbertonschool.com>
2022-06-22 19:42:38 -0500

    task4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,20 +9,15 @@
 
 void print_most_numbers(void)
 {
-	int i = 48;
+	int i;
 
-	while (i <= 57 && i >= 48)
+	for(i = 48; i < 58; i++)
 	{
-		if (i == 50 || i == 52)
+		if(i == 50 || i == 52)
 		{
-			i++;
 			continue;
 		}
-		else
-		{
-			_putchar(i);
-			i++;
-		}
+		_putchar(i);
 	}
 	_putchar('\n');
 }
