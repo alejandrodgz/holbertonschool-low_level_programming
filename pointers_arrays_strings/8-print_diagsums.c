10adb9fcbfa3cbb31c87171adee01d1138e0bcf5
alejandrodgz <5199@holbertonschool.com>
2022-06-28 22:44:37 -0500

    task

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_diagsums - fills memory with a constant byte
+ *
+ * @a: ointed area to fill
+ * @size: size
+ * Return: pointer to the memory area
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, k, j, l, size2;
+
+	i = 0;
+	l = 0;
+	k = 0;
+	size2 = size * size;
+	j = size2 - 1;
+	while (i < size2)
+	{
+		k = k + a[i];
+		i = i + (size + 1);
+	}
+	printf("%d, ", k);
+	while (j > 0)
+	{
+		j = j - (size - 1);
+		l = l + a[j];
+	}
+	printf("%d\n", l);
+}
