2a634a127e300f0733a2eab871c84a067f683571
alejandrodgz <5199@holbertonschool.com>
2022-07-04 18:42:55 -0500

    a

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Write a program that prints its name
+ *
+ * @argc: number of parameters
+ * @argv: strings
+ * Return: name of program
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
