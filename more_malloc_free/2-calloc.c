f7e42265b09158c5e981f8197b42656bce4ba211
alejandrodgz <5199@holbertonschool.com>
2022-07-08 17:10:04 -0500

    a

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates n bytes from src to dest
+ * @s1: s1
+ * @s2: s2
+ * @n: n
+ * Return: return1
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, j;
+	int *p = NULL;
+
+	if(nmemb == 0)
+		return (NULL);
+	if(size == 0)
+		return (NULL);
+	j = (nmemb * size);
+	p = malloc(j * sizeof(int));
+	if (!p)
+		return (NULL);
+	for(i = 0; i < (nmemb * size); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
