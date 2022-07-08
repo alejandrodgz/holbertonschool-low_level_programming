140aada850ea6b7b732484d63ae56de8bf52a4f6
alejandrodgz <5199@holbertonschool.com>
2022-07-08 17:51:27 -0500

    a

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -21,10 +21,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if(size == 0)
 		return (NULL);
 	j = (nmemb * size);
-	p = malloc(j * sizeof(int));
+  	p = malloc(sizeof(int) * j);
 	if (!p)
 		return (NULL);
-	for(i = 0; i < (nmemb * size); i++)
+	for (i = 0; i < j; i++)
 	{
 		p[i] = 0;
 	}
