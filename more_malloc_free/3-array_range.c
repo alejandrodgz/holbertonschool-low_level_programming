38fc3e3b86e27e7bd3c1b77d411205e556ddf00b
alejandrodgz <5199@holbertonschool.com>
2022-07-11 09:50:31 -0500

    a

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	p = malloc(sizeof(int) * max + 1);
 	if (!p)
 		return (NULL);
-	for (i = 0; i < (max); i++)
+	for (i = min; i < (max); i++)
 	{
 		p[i] = i;
 	}
