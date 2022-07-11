96ef12986abb7cfca1af95af52eea51ab45da36a
alejandrodgz <5199@holbertonschool.com>
2022-07-11 15:44:34 -0500

    a

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define SIZE 1024
+#define PI 3.14159265359
+#define ABS(x) ((x) < 0 ? x * -1 : (x) * 1)
+#endif
---
+++ preprocessor/3-function_like_macro.h~
@@ -0,0 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define SIZE 1024
+#define PI 3.14159265359
+#endif
