8e0186068c1c4589dc3f445e6f2eb4276460ba33
alejandrodgz <5199@holbertonschool.com>
2022-07-12 15:37:48 -0500

    a

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * new_dog - entry
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: aa
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog1;
+
+	dog1 = malloc(sizeof(dog_t));
+	if (dog1 == NULL)
+		return;
+	dog1->name = name;
+	dog1->age = age;
+	dog1->owner = owner;
+	return dog1;
+}
