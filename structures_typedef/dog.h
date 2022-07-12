e64be1e218caa2283aecbdbba36e0e57394ddb3a
alejandrodgz <5199@holbertonschool.com>
2022-07-12 12:23:24 -0500

    a

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _MAIN_
+#define _MAIN_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * dog - structer
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
