495f263ee212b9bb8c2de2b0d89bf8d018c08e01
alejandrodgz <5199@holbertonschool.com>
2022-07-12 16:31:17 -0500

    a

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,13 +10,35 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i;
 	dog_t *dog1;
+	char *copyname;
+	char *copyowner;
 
 	dog1 = malloc(sizeof(dog_t));
 	if (dog1 == NULL)
 		return(NULL);
-	dog1->name = name;
+	copyname = malloc(sizeof(name) + 1);
+	for (i = 0; name[i] != '\0'; i++)
+		copyname[i] = name[i];
+	if (copyname == NULL)
+	{
+		free(copyname);
+		free(dog1);
+		return(NULL);
+	}
+	copyowner = malloc (sizeof(owner) + 1);
+	for (i = 0; owner[i] != '\0'; i++)
+		copyowner[i] = owner[i];
+	if (copyowner == NULL)
+	{
+		free(copyname);
+		free(copyowner);
+		free(dog1);
+		return(NULL);
+	}
+	dog1->name = copyname;
 	dog1->age = age;
-	dog1->owner = owner;
+	dog1->owner = copyowner;
 	return dog1;
 }
