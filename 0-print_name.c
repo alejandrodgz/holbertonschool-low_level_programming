28514edd75be5b7c5c87614de82e77a486832699
alejandrodgz <5199@holbertonschool.com>
2022-07-13 16:35:10 -0500

    a

---
+++ 0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function_pointers.h"
+/**
+ * print_name - aa
+ * @name: name
+ * @f: fuction
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
