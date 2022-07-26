8fe290a3b06a855f79744e4b1114b631b5687154
alejandrodgz <5199@holbertonschool.com>
2022-07-26 17:34:14 -0500

    a

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,13 +13,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	unsigned int i = 0;
-	list_t *ptr = (list_t *)malloc(sizeof(list_t));
+	list_t *ptr;
 
+	if(strdup(str) == NULL)
+		return (NULL);
+	if(str == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(list_t));
+	if(ptr == NULL)
+		return (NULL);
 	while (str[i] != '\0')
 		i++;
 	ptr->str = strdup(str);
 	ptr->len = i;
 	ptr->next = *head;
 	*head = ptr;
-	return (*head);
+	return (ptr);
 }
