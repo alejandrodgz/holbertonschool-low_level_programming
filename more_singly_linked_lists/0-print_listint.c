b19dc59a1caba3e246ef6bb6446dbaaa04f53a8d
alejandrodgz <5199@holbertonschool.com>
2022-07-27 11:32:48 -0500

    a

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *
+ * @h: the head of the list
+ * Return: number of nodes
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *ptr;
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (0);
+	ptr = h;
+	while (ptr != NULL)
+	{
+		i++;
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+	}
+	return (i);
+}
