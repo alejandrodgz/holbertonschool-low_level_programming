1bc3dc70350e8d07989a5c5f691e62c2b70b52c6
alejandrodgz <5199@holbertonschool.com>
2022-07-27 15:01:00 -0500

    a

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,22 +1,19 @@
 #include "lists.h"
-
 /**
  * add_nodeint - eturns the number of elements in a linked listint_t list
  * @head: head of the list
  * @n: value
  * Return: the number nodes
  */
-
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *ptr;
 
-	if (*head == NULL)
-		return(NULL);
+	if (n == NULL)
+		return (NULL);
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
 		return (NULL);
-	ptr = *head;
 	ptr->n = n;
 	ptr->next = *head;
 	*head = ptr;
