f992d841885decf5853c33bb4afb7c9b42b35141
alejandrodgz <5199@holbertonschool.com>
2022-07-27 17:56:39 -0500

    a

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -9,13 +9,23 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *ptr;
-	unsigned int i = 0;
+	unsigned int i = 0, j = 0;
 
 	if (head == NULL)
 		return (NULL);
 	ptr = head;
 	if (ptr == NULL)
 		return (NULL);
+	while (head != NULL)
+	{
+		j++;
+		head = head->next;
+	}
+	if (index > j)
+	{
+		printf("(nil)");
+		return (NULL);
+	}
 	while (i < index)
 	{
 		ptr = ptr->next;
