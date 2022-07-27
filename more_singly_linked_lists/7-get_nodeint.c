8976bb4a6cfa95444fca18cea3edb1ac8f40450b
alejandrodgz <5199@holbertonschool.com>
2022-07-27 17:49:38 -0500

    a

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - eturns the number of elements in a
+ * linked listint_t list
+ * @head: head of the list
+ * @index: value
+ * Return: the number nodes
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = head;
+	if (ptr == NULL)
+		return (NULL);
+	while (i < index)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	return (ptr);
+}
