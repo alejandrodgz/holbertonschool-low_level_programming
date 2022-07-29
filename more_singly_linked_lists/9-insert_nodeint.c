8ba5756576bc00534a9b1037210dadb2e48e52d2
alejandrodgz <5199@holbertonschool.com>
2022-07-29 13:49:13 -0500

    a

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,53 +1,53 @@
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - insert a new node at a given position
- * @head: pointer to head pointer of linked list
- * @idx: index to insert new node
- * @n: new node's data
- * Return: address of new node, or NULL if failed
+ * insert_nodeint_at_index - eturns the number of elements in a
+ * linked listint_t list
+ * @head: head of the list
+ * @idx: node number
+ * @n: value
+ * Return: the number nodes
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *ptr, *pan, *pot, *pil;
+	unsigned int i = 1, j = 0;
 
-	unsigned int i = 0;
-	listint_t *new_node, *tmp;
-
-	/* account for empty pointer */
 	if (head == NULL)
 		return (NULL);
-
-	/* malloc and set values for new node */
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	pot = malloc(sizeof(listint_t));
+	if (pot = NULL)
 		return (NULL);
-	new_node->n = n;
-
-	/* account for idx 0 */
+	pot->n = n;
+	pil = *head;
+	while (pil != NULL)
+	{
+		j++;
+		pil = pil->next;
+	}
+	if (idx > j)
+	{
+		free(pot);
+		return (NULL);
+	}
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		pot->next = (*head);
+		*head = pot;
+		return (pot);
 	}
-
-	/* iterate to 1 before nth index to insert */
-	tmp = *head;
-	while (i < (idx - 1))
+	else
 	{
-		tmp = tmp->next;
-		i++;
-
-		if (tmp == NULL) /* account for idx out of range */
+		ptr = (*head)->next;
+		pan = *head;
+		while (i < idx)
 		{
-			free(new_node);
-			return (NULL);
+			ptr = ptr->next;
+			pan = pan->next;
+			i++;
 		}
+		pan->next = pot;
+		pot->next = ptr;
+		return (pot);
 	}
-
-	/* link into list */
-	new_node->next = tmp->next;
-	tmp->next = new_node;
-	return (new_node);
 }
