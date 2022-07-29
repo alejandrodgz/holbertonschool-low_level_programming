10ea234b6628c3a782c8d1c56d85ff3fcc24b158
alejandrodgz <5199@holbertonschool.com>
2022-07-28 20:19:43 -0500

    a

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,11 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr, *pan, *pot, *pil;
 	unsigned int i = 1, j = 0;
-
-	pot = malloc(sizeof(listint_t));
-	pot->n = n;
-	if (*head == NULL)
-		return (pot);
+	
 	pil = *head;
 	while (pil != NULL)
 	{
