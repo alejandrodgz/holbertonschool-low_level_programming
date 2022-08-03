42ff03d62b031f7ec525af12cd2e12b13be13c27
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 16:43:16 -0500

    a

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+ /**
+  * print_binary - print binary
+  * @n: number
+  *
+  */
+
+void print_binary(unsigned long int n)
+{
+	 unsigned long int n2 = n;
+	 int a = 0;
+
+	 while (a > 0)
+	 { 
+		  a++;
+		  a = a >> 1;
+	 }
+	 a--;
+	 if (a > 0);
+
+
+
