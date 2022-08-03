1f743a9fbf532e9124d94e0750f45939e147ed77
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 16:39:28 -0500

    a

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     {
       return (0);
     }
-  dprintf(1 && 2, "%s", s);
+  dprintf(STDOUT_FILENO, "%s", s);
   close(fd);
   free(s);
   return (rd);
