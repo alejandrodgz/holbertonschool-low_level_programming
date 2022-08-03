e69d21e2dbff780ab40b70ec1638a01b4791773f
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 15:44:37 -0500

    task 0 att

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * read_textfile - read
+  * @filename: file
+  * @letters: number of letters
+  * Return: number of characters
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+  {
+    int fd;
+    ssize_t rd;
+    char *s;
+
+    if (filename == NULL)
+      return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+      {
+        return (0);
+      }
+    s = malloc(sizeof(char) * letters + 1);
+    rd = read(fd, s, letters);
+    if (rd == -1)
+      {
+        return (0);
+      }
+    else
+      {
+        printf("%s", s);
+      }
+    return (rd);
+  }
