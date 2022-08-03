983512665121aec012a0e5790b2b3a5b5160e93f
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 16:01:36 -0500

    task 0 att 3

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
     if (filename == NULL)
       return (0);
-    fd = open(filename, O_RDONLY);
+    fd = open(filename, O_RDONLY, 600);
     if (fd == -1)
       {
         return (0);
