6be6442d98095124b04c1925a6c3fe6b5b8839f5
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 16:34:07 -0500

    a

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,26 +8,26 @@
   */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-  {
-    int fd;
-    ssize_t rd;
-    char *s;
+{
+  int fd;
+  ssize_t rd;
+  char *s;
 
-    if (filename == NULL)
+  if (filename == NULL)
+    return (0);
+  fd = open(filename, O_RDONLY, 600);
+  if (fd == -1)
+    {
       return (0);
-    fd = open(filename, O_RDONLY, 600);
-    if (fd == -1)
-      {
-        return (0);
-      }
-    s = malloc(sizeof(char) * letters + 1);
-    rd = read(fd, s, letters);
-    if (rd == -1)
-      {
-        return (0);
-      }
-    dprintf(1, "%s", s);
-    close(fd);
-    free(s);
-    return (rd);
-  }
+    }
+  s = malloc(sizeof(char) * letters + 1);
+  rd = read(fd, s, letters);
+  if (rd == -1)
+    {
+      return (0);
+    }
+  dprintf(1 && 2, "%s", s);
+  close(fd);
+  free(s);
+  return (rd);
+}
