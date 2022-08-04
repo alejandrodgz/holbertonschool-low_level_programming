ced2032b98cbfeae2e0105b1d54fd14d3dd5bb88
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 19:10:43 -0500

    a

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,23 +10,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	 int fd;
-	 ssize_t rd;
+	int fd;
+	ssize_t rd;
 
-	 if (filename == NULL)
+	if (filename == NULL)
 		 return (-1);
-	 fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	 if (fd == -1)
-	 {
-	  	 return (-1);
-	 }
-	 if (text_content == NULL)
-		 text_content = "";
-	 rd = write(fd, text_content, strlen(text_content));
-	 if (rd == -1)
-	 {
-	 	 return (0);
-	 }
-	 close(fd);
-	 return (1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+		text_content = "";
+	rd = write(fd, text_content, strlen(text_content));
+	if (rd == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	return (1);
 }
