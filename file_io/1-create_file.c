764e1b2bfd5da4d9ee84aa602b70d524e374a62f
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 19:12:45 -0500

    a

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t rd;
 
 	if (filename == NULL)
-		 return (-1);
+		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
