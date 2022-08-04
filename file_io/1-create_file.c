da7e56a675e2d602069122eb23e5e3c17eae6d1b
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 19:07:12 -0500

    a

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,6 +20,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+		text_content = "";
 	rd = write(fd, text_content, strlen(text_content));
 	if (rd == -1)
 	{
