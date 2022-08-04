92fdc50ee0fdf942a6f1651c5d52f6469eedec00
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 19:02:52 -0500

    a

---
+++ file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * create_file - read
+  * @filename: file
+  * @text_content: text to append
+  * Return: 1 on success
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	 int fd;
+	 ssize_t rd;
+
+	if (filename == NULL)
+		  return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	rd = write(fd, text_content, strlen(text_content));
+	if (rd == -1)
+	{
+		return(0);
+	}
+	close(fd);
+	return (1);
+}
---
+++ file_io/ty.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int a;
+	int b;
+
+
+    return 0;
+}
