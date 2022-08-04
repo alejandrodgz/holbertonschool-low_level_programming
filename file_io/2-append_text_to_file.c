01c5613dad115aa91f02e9e47bd24a4cd0e01b83
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-03 19:36:55 -0500

    a

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
   * Return: 1 on success
   */
 
- int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t rd;
