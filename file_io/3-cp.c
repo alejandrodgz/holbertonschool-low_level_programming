cded442230c69b0d48a74b83d8aeca523059100b
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-05 09:55:54 -0500

    a

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,10 +30,10 @@ int cp(const char *filefrom, char *fileto)
 {
 	int fd, fe, cl;
 	ssize_t rd, wd;
-	char s[1024];
+	char s[2000];
 
 	fe = open(filefrom, O_RDWR, 0664);
-	if (fe == -1)
+	if (fe = -1)
 	{
 		dprintf(2,"Error: Can\'t read from file %s\n", filefrom);
 		exit (98);
@@ -44,13 +44,13 @@ int cp(const char *filefrom, char *fileto)
 		dprintf(2,"Error: Can\'t read from file %s\n", fileto);
 		exit (98);
 	}
-	rd = read(fe, s, 1024);
+	rd = read(fe, s, 2000);
 	if (rd == -1)
 	{
 		dprintf(2,"Error: Can\'t read from file %s\n", filefrom);
 		exit (98);
 	}
-	wd = write(fd, s, 1024);
+	wd = write(fd, s, 2000);
 	if (wd == -1)
 	{
 		dprintf(2,"Error: Can\'t write to file %s\n", fileto);
