fcf7510072043522b8430973a18dea49aa35045c
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-05 10:14:04 -0500

    a

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int ac, char **av)
     if (ac != 3)
     {
         dprintf(2, "Usage: cp file_from file_to\n");
-        exit(98);
+        exit(97);
     }
     cp(av[1], av[2]);
     return (0);
@@ -28,40 +28,47 @@ int main(int ac, char **av)
 
 int cp(const char *filefrom, char *fileto)
 {
-	int fd, fe, cl;
+	int fd, fe, cl, cl2;
 	ssize_t rd, wd;
-	char s[2000];
+	char *s[1024];
 
-	fe = open(filefrom, O_RDWR, 0664);
+	fe = open(filefrom, O_RDONLY);
 	if (fe == -1)
 	{
 		dprintf(2,"Error: Can\'t read from file %s\n", filefrom);
 		exit (98);
 	}
-	fd = open(fileto, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	fd = open(fileto, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
 		dprintf(2,"Error: Can\'t read from file %s\n", fileto);
-		exit (98);
-	}
-	rd = read(fe, s, 2000);
-	if (rd == -1)
-	{
-		dprintf(2,"Error: Can\'t read from file %s\n", filefrom);
-		exit (98);
-	}
-	wd = write(fd, s, 2000);
-	if (wd == -1)
-	{
-		dprintf(2,"Error: Can\'t write to file %s\n", fileto);
 		exit (99);
 	}
-		cl = close(fd);
-		if (cl == -1)
+	while((rd = read(fe, s, 1024)) != 0)
+	{
+		if (rd == -1)
+		{
+			dprintf(2,"Error: Can\'t read from file %s\n", filefrom);
+			exit (98);
+		}
+		wd = write(fd, s, rd);
+		if (wd == -1)
+		{
+			dprintf(2,"Error: Can\'t read from file %s\n", fileto);
+			exit (99);
+		}
+	}
+	cl = close(fd);
+	if (cl == -1)
 	{
 		dprintf(2,"Error: Can't close fd %s\n", filefrom);
 		exit (100);
 	}
-	close (fe);
+	cl2 = close (fe);
+	if (cl2 == -1)
+	{
+		dprintf(2,"Error: Can't close fd %s\n", filefrom);
+		exit (100);
+	}
 	return (0);
 }
