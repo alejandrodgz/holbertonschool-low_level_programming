ec33a57a4a4766dfc4bbdc493f39dde14182c480
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-05 17:59:42 -0500

    a

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* sum_dlistint - print
+* @head: head of list
+* Return: sum of data
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+	int a = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	tmp = head;
+	while (tmp != NULL)
+	{
+		a += tmp->n;
+		tmp = tmp->next;
+	}
+	return (a);
+}
