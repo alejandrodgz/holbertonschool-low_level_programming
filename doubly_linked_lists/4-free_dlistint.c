f11367bae6bb430076a4b231f79604abeca53af3
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-05 17:24:28 -0500

    a

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_dlistint - frees list
+ * @head: head
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
