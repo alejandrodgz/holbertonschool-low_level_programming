9582bf6eed4de8bedf6df0022a53a65c598eff5c
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-05 10:29:03 -0500

    a

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -54,7 +54,7 @@ int cp(const char *filefrom, char *fileto)
 		wd = write(fd, s, rd);
 		if (wd == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: Can\'t read from file %s\n", fileto);
+			dprintf(STDERR_FILENO,"Error: Can\'t write to %s\n", fileto);
 			exit (99);
 		}
 	}
