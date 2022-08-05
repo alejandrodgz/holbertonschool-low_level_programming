3ff03d4c03a42172a1748a7161ba05602f1e6357
Alejandro <alejandro@DESKTOP-UC9D3MN>
2022-08-05 15:46:06 -0500

    a

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* print_dlistint - print
+* @h: head of list
+* Return: number of nodes
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
