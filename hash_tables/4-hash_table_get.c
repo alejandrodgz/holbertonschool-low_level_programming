58c1d18557f9a922f81dbed09482e139e9dc669d
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 16:11:10 -0500

    task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,10 +12,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
     unsigned long int index;
     hash_node_t *string;
-    
+
+    if (ht == NULL)
+            return (NULL);
     index = key_index((const unsigned char *)key, ht->size);
     string = ht->array[index];
     if (string == NULL)
             return (NULL);
+    while (string->next != NULL)
+            string = string->next;
     return (string->value);
 }
