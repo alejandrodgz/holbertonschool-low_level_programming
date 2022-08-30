7816044e4b8772b2b305f22105fdee504b840d89
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 15:46:57 -0500

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,8 +13,14 @@ hash_node_t *ht_pair(const char *key, const char *value)
     hash_node_t *entry;
 
     entry = malloc(sizeof(entry) * 1);
+    if (entry == NULL)
+            return (0);
     entry->key = malloc(strlen(key) + 1);
+    if (entry->key == NULL)
+            return (0);
     entry->value = malloc(strlen(value) + 1);
+    if (entry->value == NULL)
+            return (0);
     strcpy(entry->key, key);
     strcpy(entry->value, value);
     entry->next = NULL;
@@ -38,6 +44,8 @@ hash_node_t *ht_pair(const char *key, const char *value)
         {
             free(entry->value);
             entry->value = malloc(strlen(value) + 1);
+            if (entry->value == NULL)
+                    return (0);
             strcpy(entry->value, value);
             return (1);
         }
