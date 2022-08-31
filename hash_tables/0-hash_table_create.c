a3170b7a7db47713d0ac080e136d3da6d8aa2a50
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 19:43:36 -0500

    a

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -5,12 +5,13 @@
  *   
  * @size: size of the table
  * Return: hash table
- * 
+ *
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table1;
+
 	if (size == 0)
 		return (NULL);
 	table1 = malloc(sizeof(hash_table_t));
