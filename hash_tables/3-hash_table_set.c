c93bb7968eb46926bdcb6d720223969db69bcfc2
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 20:02:28 -0500

    a

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * create_the_node - entry for node 
+ * create_the_node - entry for node
  * @key: a key for node
  * @value: value of node
  * Return: node dir
@@ -27,13 +27,13 @@ hash_node_t *create_the_node(const char *key, const char *value)
 		free(new_node->key);
 		free(new_node);
 		return (NULL);
-        }
+	}
 	new_node->next = NULL;
 	return (new_node);
 }
 
 /**
- * hash_table_t - create entry 
+ * hash_table_t - create entry
  * @ht: table
  * @key: key of the node
  * @value: value node
@@ -45,7 +45,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *hash_node, *current;
 	char *new_value;
-	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0 || value == NULL)
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL
+	 || strlen(key) == 0 || value == NULL)
 	{
 		return (0);
 	}
@@ -53,15 +55,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	current = ht->array[index];
 	while (current != NULL)
 	{
-	if (strcmp(current->key, key) == 0)
-	{
-		new_value = strdup(value);
-		if (new_value == NULL)
-                	return (0);
-		free(current->value);
-		current->value = new_value;
+		if (strcmp(current->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+                		return (0);
+			free(current->value);
+			current->value = new_value;
 			return (1);
-	}
+		}
 	current = current->next;
 	}
 	hash_node = create_the_node(key, value);
