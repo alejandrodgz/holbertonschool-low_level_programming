95a6270ead2627190e6c2d98d1ebff755756c09b
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 19:40:25 -0500

    a

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -4,23 +4,21 @@
  * hash_table_create - creation of hash table
  *   
  * @size: size of the table
- * 
+ * Return: hash table
  * 
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table1;
-
-
-    if (size == 0)
-            return (NULL);
-    table1 = malloc(sizeof(hash_table_t));
-    if (!table1)
-            return (NULL);
-    table1->size = size;
-    table1->array = calloc((size_t)table1->size, sizeof(hash_node_t *));
-    if (table1->array == NULL)
-            return (NULL);
-    return (table1);
+	hash_table_t *table1;
+	if (size == 0)
+		return (NULL);
+	table1 = malloc(sizeof(hash_table_t));
+	if (!table1)
+		return (NULL);
+	table1->size = size;
+	table1->array = calloc((size_t)table1->size, sizeof(hash_node_t *));
+	if (table1->array == NULL)
+		return (NULL);
+	return (table1);
 }
