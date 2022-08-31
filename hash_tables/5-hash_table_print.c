9988a6c3d06e57b8a4acd29f07685faf12439fde
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 20:15:14 -0500

    a

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,35 +1,34 @@
 #include "hash_tables.h"
 
 /**
- *
- * 
- * 
+ * hash_table_print - print table
+ * @ht: table
  * 
  * 
  */
 
 void hash_table_print(const hash_table_t *ht)
 {
-    long unsigned int i;
-    hash_node_t *string;
-    char *comma = "";
+	long unsigned int i;
+	hash_node_t *string;
+	char *comma = "";
 
-    if (!ht || !ht->array)
-    {
-        return;
-    }
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        string = ht->array[i];
-        while (string)
-        {
-            if (string == NULL)
-                    continue;
-            printf("%s\'%s\': \'%s\'", comma, string->key, string->value);
-            comma = ", ";
-            string = string->next;
-        }
-    }
-    printf("}\n");
+	if (!ht || !ht->array)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		string = ht->array[i];
+		while (string)
+		{
+			if (string == NULL)
+				continue;
+			printf("%s\'%s\': \'%s\'", comma, string->key, string->value);
+			comma = ", ";
+			string = string->next;
+		}
+	}
+	printf("}\n");
 }
