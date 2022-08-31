1baf1299fe830c039554587b084a6d62c8300c02
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 20:08:20 -0500

    a

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,25 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * 
- * 
- * 
- * 
- * 
+ * hash_table_get - get info
+ * @ht: table
+ * @key: key to find node
+ * Return: value of node
+ *
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *string;
+	unsigned long int index;
+	hash_node_t *string;
 
-    if (ht == NULL)
-            return (NULL);
-    index = key_index((const unsigned char *)key, ht->size);
-    string = ht->array[index];
-    if (string == NULL)
-            return (NULL);
-    while (string->next != NULL)
-            string = string->next;
-    return (string->value);
+	if (ht == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	string = ht->array[index];
+	if (string == NULL)
+	return (NULL);
+	while (string->next != NULL)
+		string = string->next;
+	return (string->value);
 }
