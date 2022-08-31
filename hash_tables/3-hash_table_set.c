3836ae86b3973f88a946f9292c100ba633212292
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 19:59:09 -0500

    a

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,58 +1,75 @@
 #include "hash_tables.h"
+/**
+ * create_the_node - entry for node 
+ * @key: a key for node
+ * @value: value of node
+ * Return: node dir
+ */
+
 hash_node_t *create_the_node(const char *key, const char *value)
 {
-        hash_node_t *new_node;
-        new_node = malloc(sizeof(hash_node_t));
-        if (new_node == NULL)
-        {
-                return (NULL);
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
         }
-        new_node->key = strdup(key);
-        if (new_node->key == NULL)
-        {
-                free(new_node);
-                return (NULL);
-        }
-        new_node->value = strdup(value);
-        if (new_node->value == NULL)
-        {
-                free(new_node->key);
-                free(new_node);
-                return (NULL);
-        }
-        new_node->next = NULL;
-        return (new_node);
+	new_node->next = NULL;
+	return (new_node);
 }
+
+/**
+ * hash_table_t - create entry 
+ * @ht: table
+ * @key: key of the node
+ * @value: value node
+ * Return: 0 in success
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        unsigned long int index;
-        hash_node_t *hash_node, *current;
-        char *new_value;
-        if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0 || value == NULL)
-        {
-                return (0);
-        }
-        index = key_index((const unsigned char *)key, ht->size);
-        current = ht->array[index];
-        while (current != NULL)
-        {
-                if (strcmp(current->key, key) == 0)
-                {
-                        new_value = strdup(value);
-                        if (new_value == NULL)
-                                return (0);
-                        free(current->value);
-                        current->value = new_value;
-                        return (1);
-                }
-                current = current->next;
-        }
-        hash_node = create_the_node(key, value);
+	unsigned long int index;
+	hash_node_t *hash_node, *current;
+	char *new_value;
+	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0 || value == NULL)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+	if (strcmp(current->key, key) == 0)
+	{
+		new_value = strdup(value);
+		if (new_value == NULL)
+                	return (0);
+		free(current->value);
+		current->value = new_value;
+			return (1);
+	}
+	current = current->next;
+	}
+	hash_node = create_the_node(key, value);
         if (hash_node == NULL)
-        {
-                return (0);
-        }
-        hash_node->next = ht->array[index];
-        ht->array[index] = hash_node;
-        return (1);
+	{
+		return (0);
+	}
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
 }
