178c029e05ca9da3e5d051c25201029fe48b5c4d
alejandrodgz <alejo@LAPTOP-DJ0P06RS.localdomain>
2022-08-30 19:51:17 -0500

    a

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,18 +1,18 @@
 #include "hash_tables.h"
 
 /**
- * 
- * 
- * 
- * 
+ * key_index - index of a key
+ * @key: key of hash table
+ * @size: size of table
+ * Return: index
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index;
-
-    if (size == 0)
-            return (0);
-    index = hash_djb2(key);
-    return (index % size);
+	unsigned long int index;
+	
+	if (size == 0)
+	return (0);
+	index = hash_djb2(key);
+	return (index % size);
 }
